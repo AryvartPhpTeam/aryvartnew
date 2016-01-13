bb9caa5ac2a0610e43ba925b0753e02067ddd7b6
senthamizh-aryvart <senthamizhselvi@aryvart.com>
2016-01-13 15:42:59 +0530

    fix #22 footer_bug

--- wp-content/themes/aryvart/single-events.php
+++ wp-content/themes/aryvart/single-events.php
@@ -13,10 +13,12 @@
 </div>
 <?php
 while ( have_posts() ) : the_post();
+$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id), 'single-post-thumbnail' );
 ?>
 <div class="container">
-<h2 class="partner text-center"><?php the_title();?></h2>
-<p class="partner text-center"><?php the_content(); ?></p></div>
+<h4 class="partner text-center"><?php the_title();?></h4><br/>
+<div align="center"><img src="<?php _e($image[0]); ?>" class="img-responsive n-wd"> </div><br/>
+<h6 class="what-we-do1"><?php the_content(); ?></h6></div>
 <?php 
 endwhile;
 get_footer();
